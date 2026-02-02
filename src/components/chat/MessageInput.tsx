f8d71162c6732c5a1f30d3c1386d94297d470ed8
ranonla <112718385+ranonla@users.noreply.github.com>
2026-02-02 14:54:10 -0500

    add emoji picker support && enable Enter key to send messages

--- src/components/chat/MessageInput.tsx
+++ src/components/chat/MessageInput.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IoSend } from "react-icons/io5";
 import { MdInsertEmoticon } from "react-icons/md";
+import ReactionPicker from "./ReactionPicker";
 
 interface MessageInputProps {
   onSend: (message: string) => void;
@@ -8,6 +9,7 @@ interface MessageInputProps {
 
 const MessageInput = ({ onSend }: MessageInputProps) => {
   const [input, setInput] = useState("");
+  const [showEmoji, setShowEmoji] = useState(false);
 
   const handleSend = () => {
     if (!input.trim()) return;
@@ -17,19 +19,33 @@ const MessageInput = ({ onSend }: MessageInputProps) => {
 
   return (
     <div className="p-3 border-t border-gray-700 flex items-center gap-2">
-      <div className="flex-1 flex items-center bg-gray-700 rounded-xl px-3 py-2">
+      <div className="relative flex-1 flex items-center bg-gray-700 rounded-xl px-3 py-2">
+
         <input
           placeholder="Send a message..."
           value={input}
           onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSend()}
           className="w-full text-white outline-none bg-transparent"
         />
-        <button>
+
+        <button type="button" className="cursor-pointer" onClick={() => setShowEmoji((prev) => !prev)} title="Emoji">
           <MdInsertEmoticon size={20} />
         </button>
+
+        {showEmoji && (
+          <ReactionPicker
+            onEmojiSelect={(emoji) => {
+            setInput((prev) => prev + emoji);
+            setShowEmoji(false);
+            }}
+          />
+        )}
+
       </div>
 
       <button
+        type="submit"
         onClick={handleSend}
         title="Send"
         className="p-2.5 rounded bg-purple-800 hover:bg-purple-700 transition text-white cursor-pointer"
