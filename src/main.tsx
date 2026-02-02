3f260ff657bcbc91c325541262c42afb33def5bb
Ahmed Nabil <124299773+ahmniab@users.noreply.github.com>
2026-02-02 20:23:33 +0200

    feat: init react query
    * chore(deps): Add React Query and Axios dependencies to package.json
    
    * feat: wrap App component with QueryClientProvider for React Query integration
    
    * feat: add react query dev tools

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 ).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={new QueryClient()}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
