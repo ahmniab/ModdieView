476a5871ee0393d9811d984ecf97f75b5eca3cd7
ranonla <112718385+ranonla@users.noreply.github.com>
2026-02-07 13:18:39 +0200

    improve message options positioning responsiveness

--- src/components/chat/MessageList.tsx
+++ src/components/chat/MessageList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MessageItem from "./MessageItem";
 import type { Message } from "../../types";
-import { useState } from "react";
+import { useEffect, useRef ,useState } from "react";
 import type { Emoji } from "../../types";
 
 interface MessageListProps {
@@ -13,9 +13,25 @@ interface MessageListProps {
 const MessageList = ({ messages, onToggleReaction, onReply }: MessageListProps) => {
   const [activeMessageId, setActiveMessageId] = useState<string | null>(null);
   const openMessage = (id: string) => setActiveMessageId(id);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+  const isNearBottom = () => {
+  const el = containerRef.current;
+  if (!el) return true;
+  return el.scrollHeight - el.scrollTop - el.clientHeight < 100;
+  };
+
+  useEffect(() => {
+    const lastMessage = messages[messages.length - 1];
+    const didISendLastMessage = lastMessage?.isOwn;
+    if(didISendLastMessage || isNearBottom()){
+      bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages.length]);
+
 
   return (
-    <div className="flex-1 p-4 overflow-y-auto space-y-4">
+    <div className="flex-1 pt-4 pr-4 pl-4 overflow-y-auto space-y-4" ref={containerRef}>
       {messages.map((msg) => (
         <MessageItem key={msg.id} message={msg} 
           isActive={activeMessageId === msg.id}
@@ -28,6 +44,7 @@ const MessageList = ({ messages, onToggleReaction, onReply }: MessageListProps) 
           }}
            />
       ))}
+      <div ref={bottomRef} />
     </div>
   );
 };
