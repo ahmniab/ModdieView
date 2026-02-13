e35471b960b70f4a4d8a927b2fd4657a6fa6bf2d
ahmniab <ahmniab11@gmail.com>
2026-02-13 23:48:44 +0200

    feat: Define Room-related types for improved structure and organization

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,12 @@ export { EMOJIS } from "./emoji"
 export type { QuickEmoji } from "./emoji"
 export type { EmojiItem } from "./emoji"
 export type { YoutubeVideo } from "./YoutubeVideo"
+export type { 
+    Room, 
+    RoomData, 
+    Users, 
+    UserData,
+    Video, 
+    VideoPlaylist, 
+    CreatedRoom 
+} from "./room"
---
+++ src/types/room.ts
@@ -0,0 +1,31 @@
+import { type YoutubeVideo } from "./YoutubeVideo";
+
+export type Video = { url: string; } | YoutubeVideo 
+
+export type VideoPlaylist = {
+  videos: Video[];
+  currentIndex: number;
+  isPlaying: boolean;
+  videoStartTime: number;
+};
+
+export type RoomContent = VideoPlaylist | null;
+
+export interface UserData {
+  name: string;
+}
+export type Users = Record<string, UserData>;
+
+export type RoomData = {
+  roomName: string;
+  roomOwner: string;
+  users: Users;
+  roomContent: RoomContent;
+  bannedUsers?: string[];
+};
+
+export type Room = { roomId: string; } & RoomData;
+
+export type CreatedRoom = {
+  ownerWebToken: string;
+} & Room;
