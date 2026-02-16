77a190d9cce3603d04323671933a2673a6e0c7fe
ranonla <112718385+ranonla@users.noreply.github.com>
2026-02-16 23:53:38 +0200

    add hooks index barrel file

---
+++ src/hooks/index.ts
@@ -0,0 +1,4 @@
+export { useClickOutside } from "./useClickOutside";
+export { useVimeoPlayer } from "./useVimeoPlayer";
+export { default as useYoutubeSearch } from "./useYoutubeSearch";
+export { useDebounce } from "./useDebounce";
