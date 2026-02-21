087ea275fd5fb38521361f8e7804c2db3faa6f35
ranonla <112718385+ranonla@users.noreply.github.com>
2026-02-21 17:41:49 +0200

    make RoomHeader responsive and hide searchbar on mobile

--- src/components/room/RoomHeader.tsx
+++ src/components/room/RoomHeader.tsx
@@ -5,28 +5,33 @@ import { ImExit } from "react-icons/im";
 import SearchBar from '../SearchBar';
 import { IoLink } from "react-icons/io5";
 
-const RoomHeader = () => {
+const RoomHeader = ({ isBelowMd }: { isBelowMd: boolean }) => {
   return (
-    <div className="relative w-full h-14 flex items-center px-6 py-4 bg-gradient-to-r from-gray-900 to-purple-900 sticky top-0 z-50">
+    <div className="relative w-full h-14 flex items-center px-3 py-4 sm:px-6 sm:py-4
+     bg-gradient-to-r from-gray-900 to-purple-900 sticky top-0 z-50">
 
         <div className='flex gap-3 items-center'>
             <BsPersonLinesFill size={28} className='cursor-pointer'/>
             <div className='flex items-center'>
                 <Link to='/'>
-                    <img src={roomLogo} alt="ModdieView Logo" title='ModdieView' className='w-auto h-8'></img>
+                    <img src={roomLogo} alt="ModdieView Logo" title='ModdieView' className='w-auto h-6 sm:h-8'></img>
                 </Link>
-                <input type="text" placeholder='randomRoom' title='Room name' className='w-60 text-xl  text-yellow-500 font-semibold focus:outline-none p-1'></input>
+                <input type="text" placeholder='randomRoom' title='Room name' className='w-60 text-lg text-yellow-500 font-semibold focus:outline-none p-1 sm:text-xl md:text-base lg:text-xl'></input>
             </div>
         </div>
 
-        <SearchBar/>
+        {isBelowMd ? null : 
+        <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+            <SearchBar/>
+        </div>}
 
-        <div className='absolute right-0 pr-5 flex gap-5 justify-center items-center'>
+        <div className='absolute right-0 pr-2 sm:pr-5
+         flex gap-4 sm:gap-5 justify-center items-center'>
             <div className='cursor-pointer ' title='Copy'><IoLink size={30}/></div>
             <Link to="/"
                 title="Exit"
                 className='cursor-pointer'>
-                <ImExit size={23}/>
+                <ImExit className="size-6 sm:size-6"/>
             </Link>
         </div>
 
