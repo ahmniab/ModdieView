7de6d61822dfdc53bce02ba31935bf7aebdc22aa
ranonla <112718385+ranonla@users.noreply.github.com>
2026-03-08 17:21:16 +0200

    add settings modal and exit shortcut to room header

--- src/components/room/RoomHeader.tsx
+++ src/components/room/RoomHeader.tsx
@@ -1,5 +1,5 @@
 import { BsPersonLinesFill } from "react-icons/bs";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import roomLogo from "@/assets/roomLogo.png";
 import { ImExit } from "react-icons/im";
 import SearchBar from '../SearchBar';
@@ -7,18 +7,31 @@ import { IoLink } from "react-icons/io5";
 import { copyToClipboard  } from "@/utils";
 import { toast } from "react-hot-toast";
 import { useRoom } from "@/contexts/RoomContext";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
+import useKeyboardShortcut from "@/hooks/useKeyboardShortcut";
+import SettingsModal from "./SettingsModal";
 
 interface RoomHeaderPrpos {
     isBelowMd : boolean;
     roomLink: string;
     onVideoChange: (url: string) => (void);
+    toggleUsersPanel: () => void;
 }
 
 
-const RoomHeader = ({ isBelowMd, roomLink, onVideoChange }: RoomHeaderPrpos ) => {
+const RoomHeader = ({ isBelowMd, roomLink, onVideoChange, toggleUsersPanel }: RoomHeaderPrpos ) => {
   const { name, setRoomName } = useRoom();
   const [currentName, setCurrentName] = useState<string>(name);
+  const navigate = useNavigate();
+  const [showSettings, setShowSettings] = useState(false);
+  const handleKeyboardShortcut = useCallback(() => {
+    navigate("/");
+  }, [navigate]);
+
+    useKeyboardShortcut({
+    shortcutKeys: ["shift+q"],
+    callback: handleKeyboardShortcut
+    });
 
   useEffect(() => {
     if (name) {
@@ -33,26 +46,35 @@ const RoomHeader = ({ isBelowMd, roomLink, onVideoChange }: RoomHeaderPrpos ) =>
     <div className="relative w-full h-14 flex items-center px-3 py-4 sm:px-6 sm:py-4
      bg-gradient-to-r from-gray-900 to-purple-900 sticky top-0 z-50">
 
-        <div className='flex gap-3 items-center'>
-            <BsPersonLinesFill size={28} className='cursor-pointer'/>
-            <div className='flex items-center'>
-                <Link to='/'>
-                    <img src={roomLogo} alt="ModdieView Logo" title='ModdieView' className='w-auto h-6 sm:h-8'></img>
-                </Link>
-                <input 
-                    type="text" 
-                    placeholder='Random Room' 
-                    maxLength={20}
-                    title='Room name' 
-                    className='w-60 text-lg text-yellow-500 font-semibold 
-                               focus:outline-none p-1 sm:text-xl md:text-base lg:text-xl' 
-                    value={currentName} 
-                    onChange={(e) => handleNameChange(e.target.value)} 
-                    onBlur={() => {
-                        setRoomName(currentName);                    
-                    }}
-                />
-            </div>
+        <div className='relative flex gap-3 items-center'>
+          {!isBelowMd &&
+            <button className='cursor-pointer' title="Manage Profile"
+              onMouseDown={(e) => {
+                e.stopPropagation();
+                setShowSettings((prev) => !prev);
+            }}>
+              <BsPersonLinesFill size={28}/>
+            </button>
+          }
+
+          <div className='flex items-center'>
+              <Link to='/'>
+                  <img src={roomLogo} alt="ModdieView Logo" title='ModdieView' className='w-auto h-6 sm:h-8'></img>
+              </Link>
+              <input 
+                  type="text" 
+                  placeholder='Random Room' 
+                  maxLength={20}
+                  title='Room name' 
+                  className='w-60 text-lg text-yellow-500 font-semibold 
+                              focus:outline-none p-1 sm:text-xl md:text-base lg:text-xl' 
+                  value={currentName} 
+                  onChange={(e) => handleNameChange(e.target.value)} 
+                  onBlur={() => {
+                      setRoomName(currentName);                    
+                  }}
+              />
+          </div>
         </div>
 
         {isBelowMd ? null : 
@@ -74,12 +96,14 @@ const RoomHeader = ({ isBelowMd, roomLink, onVideoChange }: RoomHeaderPrpos ) =>
             </button>
                                 
             <Link to="/"
-                title="Exit"
+                title="Exit (shift+q)"
                 className='cursor-pointer'>
                 <ImExit className="size-6 sm:size-6"/>
             </Link>
         </div>
-
+        {showSettings && (
+          <SettingsModal onClose={() => setShowSettings(false)} toggleUsersPanel={toggleUsersPanel}/>
+        )}
     </div>
   )
 }
