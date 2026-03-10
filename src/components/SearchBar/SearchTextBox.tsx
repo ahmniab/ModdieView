0a358512ab3bbb01a6ae8577ed19bbde0d3635a0
ranonla <112718385+ranonla@users.noreply.github.com>
2026-03-11 01:00:29 +0200

    update SearchTextBox border radius

--- src/components/SearchBar/SearchTextBox.tsx
+++ src/components/SearchBar/SearchTextBox.tsx
@@ -19,7 +19,7 @@ const SearchTextBox: RC.FC<{
         callback: handleKeyboardShortcut
     });
     return (
-        <div className='w-[330px] h-11 sm:w-[390px] sm:h-10 md:w-[320px] md:h-10 lg:w-[360px] lg:h-8 bg-gray-600 border border-black rounded-2xl p-4 flex items-center gap-2 outline-none cursor-pointer'>
+        <div className='w-[330px] h-11 sm:w-[390px] sm:h-10 md:w-[320px] md:h-10 lg:w-[360px] lg:h-8 bg-gray-600 border border-black rounded-xl p-4 flex items-center gap-2 outline-none cursor-pointer'>
             <LiaSearchSolid className="size-7 sm:size-6"/>
             <input 
                 type="text"
